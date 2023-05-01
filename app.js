d9688a146e3dd1bd407915de1445a230a94ae457
gkk <gkk.dev.ops@gmail.com>
2023-05-01 19:57:39 +0200

    Move config to .env

--- app.js
+++ app.js
@@ -1,59 +1,16 @@
 const express = require("express");
-const { randomUUID } = require('crypto');
-const cors = require('cors');
+const { randomUUID } = require("crypto");
+const cors = require("cors");
+require('dotenv').config()
 
 const app = express();
 
-// or specify a whitelist of allowed origins
-app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000']
-  }));
-app.use(express.json())
-
-// Dummy data
-let contacts = [
-  {
-    id: randomUUID(),
-    first: "John",
-    last: " Doe",
-    favorite: true,
-    phone: "+1 123-456-7890",
-    avatar: "https://loremflickr.com/640/360",
-    twitter: "johndoe",
-    notes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-  },
-  {
-    id: randomUUID(),
-    first: "Jane",
-    last: "Smith",
-    favorite: false,
-    phone: "+44 7911 123456",
-    avatar: "https://loremflickr.com/640/360",
-    twitter: "janesmith",
-    notes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet nibh ultrices, finibus elit vitae, viverra nisl. Fusce vel sapien eget dolor egestas facilisis eu ut ante."
-  },
-  {
-    id: randomUUID(),
-    first: "David",
-    last: "Kim",
-    favorite: true,
-    phone: "+81 3-1234-5678",
-    avatar: "https://loremflickr.com/640/360",
-    twitter: "davidkim",
-    notes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut ante sapien. Fusce mollis dolor eget sem faucibus, a fermentum tellus aliquam. Praesent varius, mi in congue consequat, ipsum nisi malesuada nisi, eu convallis turpis arcu ut urna."
-  },
-  {
-    id: randomUUID(),
-    first: "Anna",
-    last: "Garcia",
-    favorite: false,
-    phone: "+55 11 98765-4321",
-    avatar: "https://loremflickr.com/640/360",
-    twitter: "annagarcia",
-    notes: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi. Nam aliquam tristique elit, sit amet auctor lectus consectetur vel. Vivamus vel arcu sit amet nulla vestibulum rhoncus. Sed ultricies, purus quis tempor lacinia, quam nibh vehicula nulla, vitae hendrerit turpis augue at nisl."
-  }
-];
-console.log("Test data:\n", contacts, "\n")
+app.use(
+  cors({
+    origin: [`http://${process.env.CLIENT_URL}:${process.env.CLIENT_PORT}`, `http://${process.env.SERVER_URL}:${process.env.SERVER_PORT}`],
+  })
+);
+app.use(express.json());
 
 // Endpoint to get all contacts
 app.get("/api/contacts", (req, res) => {
@@ -64,7 +21,16 @@ app.get("/api/contacts", (req, res) => {
 app.post("/api/contacts", (req, res) => {
   const { first, last, favorite, phone, avatar, twitter, notes } = req.body;
   const id = randomUUID();
-  const newContact = { id, first, last, favorite, phone, avatar, twitter, notes };
+  const newContact = {
+    id,
+    first,
+    last,
+    favorite,
+    phone,
+    avatar,
+    twitter,
+    notes,
+  };
   contacts.push(newContact);
   res.json(newContact);
 });
@@ -82,23 +48,17 @@ app.get("/api/contacts/:id", (req, res) => {
 // Endpoint to update an existing contact by ID
 app.put("/api/contacts/:id", (req, res) => {
   const { id } = req.params;
-  const { first, last, favorite, phone, avatar, twitter, notes } = req.body;
-  const contact = contacts.find((c) => c.id === id);
-  if (!contact) {
+  const update = req.body;
+
+  const contactIndex = contacts.findIndex((c) => c.id === id);
+  if (contactIndex === -1) {
     return res.status(404).json({ message: "Contact not found" });
   }
-  contact.first = first || contact.first;
-  contact.last = last || contact.last;
-  if(favorite === undefined) {
-    contact.favorite = contact.favorite;
-  } else {
-      contact.favorite = favorite;
-  }
-  contact.phone = phone || contact.phone;
-  contact.avatar = avatar || contact.avatar;
-  contact.twitter = twitter || contact.twitter;
-  contact.notes = notes || contact.notes;
-  res.json(contact);
+
+  const updatedContact = { ...contacts[contactIndex], ...update };
+  contacts[contactIndex] = updatedContact;
+
+  res.json(updatedContact);
 });
 
 // Endpoint to delete an existing contact by ID
@@ -113,6 +73,6 @@ app.delete("/api/contacts/:id", (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log("Server started on port 3000");
-  console.log("\nhttp://127.0.0.1:3000");
+  console.log(`Server started on port ${process.env.PORT}`);
+  console.log(`\nhttp://${process.env.SERVER_URL}:${process.env.SERVER_PORT}`);
 });
